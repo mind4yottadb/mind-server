d791b9e8e0ff7cf08a2dfff31d1fc9ee4fa8405d
slalli <stefano@dnasoft.nl>
2026-01-11 21:23:47 +0100

    #53 test structure

---
+++ test/m/mut.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#################################################################
+#                                                               #
+# Copyright (c) 2024 YottaDB LLC and/or its subsidiaries.       #
+# All rights reserved.                                          #
+#                                                               #
+#    This source code contains the intellectual property	      #
+#    of its copyright holder(s), and is made available	        #
+#    under a license.  If you do not know the terms of	        #
+#    the license, please stop and do not read further.	        #
+#                                                               #
+#################################################################
+
+exitCode=0
+#if ! yottadb -r ^%ydbxiderTestApiString; then
+#	exitCode=1
+#fi
+if ! yottadb -r ^%ydbxiderTestApiHash; then
+	exitCode=1
+fi
+#if ! yottadb -r ^%ydbxiderTestApiTtl; then
+#	exitCode=1
+#fi
+
+exit $exitCode
+

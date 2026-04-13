bfa42c2dffcad8395917b045ccc20c624ce0b1e0
slalli <stefano@dnasoft.nl>
2026-04-13 13:19:27 +0200

    fix: remote IP number disappeared, got restored now

--- src/server/m/socket-server/sessions/_mindServerSession.m
+++ src/server/m/socket-server/sessions/_mindServerSession.m
@@ -63,7 +63,7 @@ start ;
 	; extract the remoteIp #
 	zshow "d":devtmp
 	for i=0:0 set i=$order(devtmp("D",i)) quit:'i  set:devtmp("D",i)["REMOTE" %remoteIp=$zpiece($zpiece(devtmp("D",i),"REMOTE=",2),"@")
-	set %remoteIp="" ;$piece(%remoteIp,":",4)
+	set %remoteIp=$piece(%remoteIp,":",4)
 	;
 	; populate the session node
 	set params("type")="S",params("description")="Socket clientId "_$job,params("ipNumber")=%remoteIp

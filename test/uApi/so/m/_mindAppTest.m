4049ef642e0f6061809413bd76750b88060e5c4b
slalli <stefano@dnasoft.nl>
2026-05-07 11:11:42 +0200

    test: new .so created for testing hooks

--- test/uApi/so/m/_mindAppTest.m
+++ test/uApi/so/m/_mindAppTest.m
@@ -3,6 +3,24 @@ soTest
     ;
     set a=1
     ;
+    quit a
+    ;
+    ;
+onInit
+    set ^%mindSo=1
+    ;
+    quit
+    ;
+    ;
+onTerminate
+    set ^%mindSo=0
+    ;
+    quit
+    ;
+    ;
+onError
+    set ^%mindSo=2
+    ;
     quit
     ;
     ;

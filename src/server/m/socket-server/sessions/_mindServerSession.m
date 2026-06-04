a2b69bc051567522174da4994fdbde20820d3c22
slalli <stefano@dnasoft.nl>
2026-06-04 14:58:08 +0200

    refactor: better way to get socket remote address

--- src/server/m/socket-server/sessions/_mindServerSession.m
+++ src/server/m/socket-server/sessions/_mindServerSession.m
@@ -68,9 +68,7 @@ start ;
 	; create a new session node (to be filled by the handshaking)
 	; ----------------------
 	; extract the remoteIp #
-	zshow "d":devtmp
-	for i=0:0 set i=$order(devtmp("D",i)) quit:'i  set:devtmp("D",i)["REMOTE" %mindRemoteIp=$zpiece($zpiece(devtmp("D",i),"REMOTE=",2),"@")
-	set %mindRemoteIp=$piece(%mindRemoteIp,":",4)
+	set %mindRemoteIp=$piece($zsocket($ZPIN,"REMOTEADDRESS",0),":",4,99)
 	;
 	; populate the session node
 	set params("type")="S",params("description")="Socket clientId "_$job,params("ipNumber")=%mindRemoteIp
